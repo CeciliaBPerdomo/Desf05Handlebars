66db98d75a28a4b79f7ead98192f3f40c68a13a4
Cecilia <cecilia.perdomo@gmail.com>
2022-04-07 07:20:28 -0300

    fin

--- src/main.js
+++ src/main.js
@@ -7,21 +7,28 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 const app = express()
+let productos = []
 
 app.engine('handlebars', exphbs.engine())
+
+app.use(express.urlencoded({ extended: true }))
+app.set('views', './views')
 app.set('view engine', 'handlebars')
 
-app.set('views', './views')
-
-app.get('/productos', (req, res) => {
-    let productos = [
-        {nombre: 'Escuadra', precio: 20, foto: "unafoto"}, 
-        {nombre: 'Regla', precio: 10, foto: "otrafoto"}, 
-        {nombre: 'Compás', precio: 20, foto: "masfoto"}
-    ]
-    res.render('productos', { productos })
+app.get('/', (req, res) => {
+    res.render('datos')
 })
 
+app.post('/productos', (req, res) => {
+    let productos = [
+        {nombre: 'Escuadra', precio: 20, foto: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Squadra_45.jpg/640px-Squadra_45.jpg"}, 
+        {nombre: 'Regla', precio: 10, foto: "https://image.shutterstock.com/image-vector/school-measuring-plastic-ruler-20-260nw-615662024.jpg"}, 
+        {nombre: 'Compás', precio: 20, foto: "https://thumbs.dreamstime.com/b/comp%C3%A1s-de-dibujo-aislado-rojo-132996590.jpg"}
+    ]
+    productos.push(req.body)
+    console.log(productos)
+    res.render('productos', { productos })
+})
 
 /* ---- Server Listen ----- */
 const PORT = 8080
